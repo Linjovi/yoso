3c8cd6672a291d77268bd87ba61a4e8f507662fa
jovi <linjuewei@corp.netease.com>
2019-07-15 14:59:46 +0800

    fix(init.action.ts): fix the bug when init without path
    fix #9

--- actions/init.action.ts
+++ actions/init.action.ts
@@ -8,83 +8,65 @@ import { optionView } from "../ui/optionInput";
 import { initSelector } from "../ui/initSelector";
 
 interface initData {
-  username: string,
-  repo: string,
-  branch: string,
-  download: string,
-  path: string
+  username: string;
+  repo: string;
+  branch: string;
+  download: string;
+  path: string;
 }
 
 interface initUIValue {
   /**
    * Selected template name
    */
-  tpl: string,
+  tpl: string;
   /**
    * The value of path option
    */
-  path: string
+  path: string;
 }
-
-var filePath = path.dirname(__dirname); //yoso根目录
 
 export class InitAction extends AbstractAction {
   public async handle(inputs: NewCmd) {
-    isRewrite(inputs.path, function () {
-      initTpl(inputs);
+    var config = readConfig();
+
+    var data = {
+      username: inputs.options.username || config.username,
+      repo: inputs.options.repo || config.repo,
+      branch: inputs.options.branch || config.branch || "master",
+      download: inputs.tpl,
+      path: inputs.path
+    };
+
+    //if use init directly
+    if (!data.download) {
+      showInitUI(data);
+      return;
+    }
+
+    isRewrite(data.path, () => {
+      initTpl(data, inputs.options.others);
     });
   }
-
 }
 /**
  * Show init GUI
- * @param data 
+ * @param data
  */
 function showInitUI(data: initData) {
   initSelector({
-    ...data, onSubmit: (value: initUIValue) => {
+    ...data,
+    onSubmit: (value: initUIValue) => {
       data.download = value.tpl;
-      data.path = value.path || value.tpl;
 
-      var name = path.basename(data.path);
-      const gitInfo = getGitInfo();
-
-      var options: { [k: string]: any } = { name, date: formatDate(new Date()) };
-
-      if (gitInfo.name) {
-        options.author = gitInfo.name;
-      }
-      if (gitInfo.email) {
-        options.email = gitInfo.email;
-      }
-
-      optionView(options, (list: any) => {
-        list.forEach((element: any) => {
-          options[element.key] = element.value;
-        });
-        console.log(options);
-        requestUrl(data.username, data.repo, data.branch, data.download, data.path, options);
-      });
-      return;
+      initTpl(data, true);
     }
-  })
+  });
 }
 
-
-function initTpl(inputs: NewCmd) {
-  var config = readConfig();
-
-  var data = {
-    username: inputs.options.username || config.username,
-    repo: inputs.options.repo || config.repo,
-    branch: inputs.options.branch || config.branch || "master",
-    download: inputs.tpl,
-    path: inputs.path || inputs.tpl
-  };
-  //if use init directly
-  if (!data.path) {
-    showInitUI(data)
-    return
+function initTpl(data: initData, others: boolean) {
+  if (!data.path && data.download) {
+    data.path = path.basename(data.download).split(".")[0];
   }
   var name = path.basename(data.path);
   const gitInfo = getGitInfo();
@@ -98,16 +80,30 @@ function initTpl(inputs: NewCmd) {
     options.email = gitInfo.email;
   }
 
-  if (inputs.options.others) {
+  if (others) {
     optionView(options, (list: any) => {
       list.forEach((element: any) => {
         options[element.key] = element.value;
       });
       console.log(options);
-      requestUrl(data.username, data.repo, data.branch, data.download, data.path, options);
+      requestUrl(
+        data.username,
+        data.repo,
+        data.branch,
+        data.download,
+        data.path,
+        options
+      );
     });
     return;
   }
 
-  requestUrl(data.username, data.repo, data.branch, data.download, data.path, options);
+  requestUrl(
+    data.username,
+    data.repo,
+    data.branch,
+    data.download,
+    data.path,
+    options
+  );
 }
