3cb27f8851eaac7285018b94befc0ec35c56579a
刘放 <362512489@qq.com>
2019-08-07 15:07:39 +0800

    remove useless code of config action

--- actions/config.action.ts
+++ actions/config.action.ts
@@ -1,10 +1,5 @@
 import { AbstractAction } from "./abstract.action";
 import { configView } from "../ui/config";
-import { config } from "rxjs";
-import * as fs from "fs";
-import * as path from "path";
-
-const filePath = path.dirname(__dirname); //yoso根目录
 
 export class ConfigAction extends AbstractAction {
   public async handle() {
